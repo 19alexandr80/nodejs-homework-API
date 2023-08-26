5bfb6e66c04d93a68f49477d9aa0d442ac908d77
19alexandr80 <usanka1980@gmail.com>
2023-08-26 15:02:48 +0300

    valid

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -11,18 +11,25 @@ const router = express.Router();
 const contactSchema = Joi.object({
   name: Joi.string()
     .pattern(
-      new RegExp("^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$")
+      new RegExp("^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"),
+      { name: "alpha" }
     )
     .required()
     .messages({
       "any.required": "missing required name field",
+      "string.pattern.name": "name not valid",
     }),
-  email: Joi.string().required().messages({
+  email: Joi.string().email().required().messages({
     "any.required": "missing required email field",
+    "string.email": "email must be a valid email :)",
   }),
-  phone: Joi.string().required().messages({
-    "any.required": "missing required phone field",
-  }),
+  phone: Joi.string()
+    .pattern(/^[0-9: -/+]+$/, "numbers")
+    .required()
+    .messages({
+      "any.required": "missing required phone field",
+      "string.pattern.name": "phone numbers not valid",
+    }),
 });
 
 router.get("/", async (req, res, next) => {
