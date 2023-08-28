aab24d3fb700b9d086e42fff624d8b64bce3274b
19alexandr80 <usanka1980@gmail.com>
2023-08-28 13:43:20 +0300

    ren

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,31 +1,5 @@
-// const Joi = require("joi");
-
 const contactsApi = require("../models/contacts");
 const { HttpError, ctrlWrapper } = require("../helpers");
-
-// const contactSchema = Joi.object({
-//   name: Joi.string()
-//     .pattern(
-//       new RegExp("^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"),
-//       { name: "alpha" }
-//     )
-//     .required()
-//     .messages({
-//       "any.required": "missing required name field",
-//       "string.pattern.name": "name not valid",
-//     }),
-//   email: Joi.string().email().required().messages({
-//     "any.required": "missing required email field",
-//     "string.email": "email must be a valid email :)",
-//   }),
-//   phone: Joi.string()
-//     .pattern(/^[0-9: -/+]+$/, "numbers")
-//     .required()
-//     .messages({
-//       "any.required": "missing required phone field",
-//       "string.pattern.name": "phone numbers not valid",
-//     }),
-// });
 
 const getAllContacts = async (_, res) => {
   const data = await contactsApi.listContacts();
@@ -40,10 +14,6 @@ const getContactById = async (req, res) => {
   res.json(data);
 };
 const addContact = async (req, res) => {
-  //   const { error } = contactSchema.validate(req.body);
-  //   if (error) {
-  //     throw HttpError(400, error.details[0].message);
-  //   }
   const data = await contactsApi.addContact(req.body);
   if (!data) {
     throw HttpError(404, "Not found");
@@ -64,10 +34,6 @@ const changeContact = async (req, res) => {
   if (Object.keys(req.body).length === 0) {
     throw HttpError(400, `mising required`);
   }
-  // const { error } = contactSchema.validate(req.body);
-  // if (error) {
-  //   throw HttpError(400, error.details[0].message);
-  // }
   const id = req.params.contactId;
   const data = await contactsApi.updateContact(id, req.body);
   if (!data) {
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Alexandr:eez1buKqGiREs4Xh@cluster0.vwoosru.mongodb.net/contact_book?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+    console.log("oookkk");
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
